250b53cf0cd76e6407ec1400a5b6881873f15660
Nick Brill <nick.brill@sportngin.com>
2016-09-20 12:56:15 -0500

    updates

--- app/src/main/java/com/example/android/huntingapp/MainActivity.java
+++ app/src/main/java/com/example/android/huntingapp/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.android.huntingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        RelativeLayout main_view = (RelativeLayout) findViewById(R.id.main_view);
+
+        switch(item.getItemId()) {
+            case R.id.menu_red:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu_green:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.menu_yellow:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.YELLOW);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increase(View view) {
+        if (quantity == 6) {
+            Toast.makeText(MainActivity.this, "It is illegal to shoot more than the daily limit", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrease(View view) {
+        if (quantity == 1) {
+            Toast.makeText(MainActivity.this, "You can't shoot less than 1 duck or goose", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+    public void displayQuantity(int quantity) {
+
     }
 }
